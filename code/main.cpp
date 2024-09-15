68b656205b856df3687a9cd3ba387addc99c8c26
David Phume <davidphume@gmail.com>
2024-09-15 22:17:49 +0200

    my 3rd commit added vs code

---
+++ code/main.cpp
@@ -0,0 +1,63 @@
+
+
+
+// Afrobotics.com
+// Create a matrix and add it's values by adding two objects together.
+
+
+#include <iostream>
+using namespace std;
+
+/*
+
+class comp {
+
+    public:
+
+    //variables
+    int size;
+    int value;
+    int *arr;
+
+    //parameterized constructor
+    comp(int s = 0 , int v = 0)
+    : size(0), value(0)
+    
+    {
+        arr = new int [size];
+        for (int x = 0 ; x < size ; x++){
+            arr[x] = value;
+        }
+    }
+
+    //copy constructor
+
+
+    //overloaded operator
+
+
+    //print
+    void print (){
+
+       for (int x = 0 ; x < size ; x++){
+           cout  <<  arr[x] << "";
+        }
+
+    }
+     
+
+};
+
+*/
+
+
+int main (){
+
+
+cout << "I am a maker and robotist and scientist" << endl;
+
+
+    return 0;
+}
+
+
